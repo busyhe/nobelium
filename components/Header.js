0495e72dad4cf83335b6ae2e9d1f3cf37c78c4ba
busyhe <busyhe@qq.com>
2026-04-02 14:33:30 +0800

    fix: notion api 429

--- components/Header.js
+++ components/Header.js
@@ -13,8 +13,8 @@ const NavBar = () => {
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 2, name: locale.NAV.RSS, to: '/feed', show: false, external: true },
     { id: 3, name: locale.NAV.ME, to: 'http://busyhe.com/', show: true, external: true },
-    { id: 4, name: locale.NAV.WIKI, to: 'https://wiki.busyhe.com/', show: true, external: true },
-    { id: 5, name: locale.NAV.SEARCH, to: '/search', show: true }
+    // { id: 4, name: locale.NAV.WIKI, to: 'https://wiki.busyhe.com/', show: true, external: true },
+    // { id: 5, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
